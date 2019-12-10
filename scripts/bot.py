fdc60772a380ce2ec5763a10e277a13b07d91033
Coltri, David W <coltri@rowan.edu>
2019-12-09 23:03:45 -0500

    added output flag

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+import os
 import gpt_2_simple as gpt2
 import fire
-import os
 import tweepy
 import tqdm
 from pathlib import Path
@@ -14,10 +14,10 @@ load_dotenv(dotenv_path=str(Path("..") / ".env"))
 
 def generate_tweets(run):
     """
-    Generate a couple tweets from our already 
-    trained and tuned model 
+    Generate a couple tweets from our already
+    trained and tuned model
     """
-    # Ensure that the path is correct, even for cron... 
+    # Get our pathfor the checkpoint setup
     checkpoint_dir = Path("checkpoint").absolute()
 
     # Start tensorflow session & load our model
@@ -25,28 +25,43 @@ def generate_tweets(run):
     gpt2.load_gpt2(sess, checkpoint_dir=checkpoint_dir, run_name=run)
 
     # Generate and return tweets
-    tweets = list(map(lambda x: x.strip(), gpt2.generate(sess, checkpoint_dir=checkpoint_dir, run_name=run, return_as_list=True)[0].split("\n")))
+    tweets = list(
+        map(
+            lambda x: x.strip(),
+            gpt2.generate(
+                sess, checkpoint_dir=checkpoint_dir, run_name=run, return_as_list=True
+            )[0].split("\n"),
+        )
+    )
     return tweets
 
 
-def post_tweets(run='run1'):
-    in_tweet_list = generate_tweets(run)
-
+def post_tweet(tweet):
     # Authenticate to Twitter
     auth = tweepy.OAuthHandler(os.getenv("twitter_api"), os.getenv("twitter_secret"))
-    auth.set_access_token(os.getenv("twitter_access_token"), os.getenv("twitter_access_token_secret"))
+    auth.set_access_token(
+        os.getenv("twitter_access_token"), os.getenv("twitter_access_token_secret")
+    )
 
     # Create API object
     api = tweepy.API(auth)
+    try:
+        api.update_status(tweet)
+    except tweepy.error.TweepError:
+        pass
+
+
+def main(run="run1", output="twitter"):
+    in_tweet_list = generate_tweets(run)
 
     # Create a tweet
     for tweet in set(in_tweet_list):
-        try:
-            api.update_status(tweet)  
-        except tweepy.error.TweepError:
-            pass
+        if output == "twitter":
+            post_tweet(tweet)
+        else:
+            print(tweet)
 
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
-    fire.Fire(post_tweets)
+    fire.Fire(main)
